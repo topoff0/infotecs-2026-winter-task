8a105daa5533c229a91ac7269ee6ea0d74c8638c
sweeppy <104524272+sweeppy@users.noreply.github.com>
2026-02-01 20:48:57 +0300

    Add base repositories interfaces

---
+++ Chronos.Core/Repositories/Common/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Chronos.Core.Repositories.Common;
+
+public interface IRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(Guid id, CancellationToken token = default);
+    Task<IEnumerable<T>> GetAllAsync(CancellationToken token = default);
+    Task AddAsync(T entity, CancellationToken token = default);
+    void Update(T entity);
+    void Delete(T entity);
+}
---
+++ Chronos.Core/Repositories/Common/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace Chronos.Core.Repositories.Common;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(CancellationToken token = default);
+}
