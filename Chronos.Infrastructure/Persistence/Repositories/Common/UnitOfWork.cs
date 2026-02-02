ad1b3d6d1f76cae28e67cba1124fef32e453c06b
sweeppy <104524272+sweeppy@users.noreply.github.com>
2026-02-02 11:58:09 +0300

    Implement IUnitOfWork

---
+++ Chronos.Infrastructure/Persistence/Repositories/Common/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Chronos.Core.Repositories.Common;
+
+namespace Chronos.Infrastructure.Persistence.Repositories.Common;
+
+public class UnitOfWork(ChronosDbContext context) : IUnitOfWork
+{
+    private readonly ChronosDbContext _context = context;
+    public async Task<int> SaveChangesAsync(CancellationToken token = default)
+    {
+        return await _context.SaveChangesAsync(token);
+    }
+}
